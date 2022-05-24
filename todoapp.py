3811b019a0458280b9dad7183b1836805ac893c6
AlexSG <alexanderstopper@hotmail.com>
2022-05-24 18:03:06 -0500

    Primeros archivos App

---
+++ todoapp.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+                
+@app.route('/') #Será responsable de mostrar la lista actual de tareas pendientes en una tabla HTML.
+def index():    # y mostrar un formulario HTML para enviar un nuevo elemento de la lista de tareas pendientes.
+   return render_template('base.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
