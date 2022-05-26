b71c7fc72457ecdaa816c23770f5815fe8789f78
AlexSG <alexanderstopper@hotmail.com>
2022-05-25 21:41:11 -0500

    Cambio total

--- todoapp.py
+++ todoapp.py
@@ -1,13 +1,10 @@
-from turtle import title
 from flask import Flask, render_template
-
-
 
 app = Flask(__name__)
 
 @app.route('/') #Será responsable de mostrar la lista actual de tareas pendientes en una tabla HTML.
 def index():    # y mostrar un formulario HTML para enviar un nuevo elemento de la lista de tareas pendientes.
-   return render_template('base.html')
+   return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
